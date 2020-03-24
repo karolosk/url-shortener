6fec5adc80363de83f87b10e45c9d82b472c5503
karolosk <karolos.koutsoulelos@gmail.com>
2020-03-24 14:49:19 +0200

    Changing venv

--- app.py
+++ app.py
@@ -1,28 +1,62 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import session
 import os
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
-from models.url_model import Url
+
+
+if os.getenv("ENV") == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:    
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Url(db.Model):
+
+    __tablename__ = 'url'
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.Text())
+    shorted_url = db.Column(db.Text())
+    created = db.Column(db.Date())
+
+
+    def __init__(self, original_url, shorted_url):
+        self.original_url = original_url
+        self.shorted_url = shorted_url
+        self.created = date.today()
 
 
 @app.route('/',  methods=['GET', 'POST'])
-def homme():
-    print(request.method)
-    # service = UrlService()
-    # all = service.get_all()
-    if request.method == 'GET':
-        return render_template('home.html' ) #,all_urls = all)
-
+def home():
     
-    data = request.form['original-url']
-    print(data)
-    new = Url(data, data)
+    return render_template('home.html' )
 
-    session.add(new)
-    session.commit()
-    return render_template('home.html') #,all_urls = all) ,all_urls = all)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        original_url = request.form['original-url']
+        # print(original_url)
+        
+        if original_url == '':
+            return render_template('home.html', message='Please enter url to shotrten')
+        
+        if db.session.query(Url).filter(Url.original_url == original_url).count() == 0:
+            data = Url(original_url, original_url)
+            db.session.add(data)
+            db.session.commit()
+
+            return render_template('test.html')
+        return render_template('home.html', message='Url already exists')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
